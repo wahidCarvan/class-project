2f47e6c0dfcf73fb2d5b852fe8286d1b71a4d106
wahidc7101 <wahidc7101@gmail.com>
2016-05-25 08:23:31 -0400

    intial commit

---
+++ games.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	var character = {
+		tom: {
+			name: "tom", 
+			health: 100,
+			attack: 5,
+			imageurl: "assets/images/'filename", // add a saved image
+			enemyAttackBack: 15,
+		},
+
+	// do the above code 3 more times so you can 4 characters
+
+
+
+// wacth the lectures from when they are building html with jquery 5.
+
+
+	}
+
+	// make variables to appear on the page
+	var characterDiv = '<div class="character" data-name="' + character.name + '">';
+	$('#character-choices').append(characterDiv);
+
+
+
+
+
+});
+
+
+// first
+
